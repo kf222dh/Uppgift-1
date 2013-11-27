164f7e1c656372ab33bf00dbf43bdee30465f5d3
Kevin Fredriksson <kf222dh@student.lnu.se>
2013-11-28 00:00:56 +0100

    Mer testning kommer

---
+++ Triangel/kod/c#/Tri/UnitTestProjectTri/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectTri
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void NegativeInputDoubles()
+        {
+            Triangle t = new Triangle(1.5, -2.5, 1.5);
+        }
+
+        [TestMethod]
+
+        public void BadTriangle()
+        {
+            Triangle t = new Triangle(1.0, 2.0, 3.0);
+        }
+
+        [TestMethod]
+
+        public void EquitateralFalseDoubles_1()
+        {
+            Triangle t = new Triangle(3.6, 4.0, 5.7);
+            Assert.IsTrue(!t.isEquilateral());
+        }
+               
+        [TestMethod]
+
+        public void IsoscelesFalseDoubles_1()
+        {
+            Triangle t = new Triangle(5.6, 6.0, 7.7);
+            Assert.IsTrue(!t.isIsosceles());
+        }
+
+        [TestMethod]
+
+        public void ScaleneTrueDoubles_1()
+        {
+            Triangle t = new Triangle(5.6, 6.0, 7.7);
+            Assert.IsTrue(t.isScalene());
+        }
+    }
+}
